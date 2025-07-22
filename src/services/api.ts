974a2bf7ae75dde469071e60962b0f66b0127483
dq-aem-anilkumar <anilkumarg@digiquadsolutions.com>
2025-07-22 18:17:42 +0530

    Updated api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://192.168.1.34:8081';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://192.168.1.30:8081';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
